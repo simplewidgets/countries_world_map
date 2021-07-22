f48543f0318659c398221e1b4887c6b672d3c602
Lau Dijksterhuis <49369680+LauDijksterhuis@users.noreply.github.com>
2021-07-22 22:04:35 +0200

    Added Country name to codes

--- lib/countries_colors.dart
+++ lib/countries_colors.dart
@@ -6,256 +6,756 @@ import 'package:flutter/material.dart';
 ///
 /// For example: uS = United States, nL = Netherlands, jP = Japan.
 class CountryColors {
+  /// Andorra
   final Color? aD;
+
+  /// United Arab Emirates (the)
   final Color? aE;
+
+  /// Afghanistan
   final Color? aF;
+
+  /// Antigua and Barbuda
   final Color? aG;
+
+  /// Anguilla
   final Color? aI;
+
+  /// Albania
   final Color? aL;
+
+  /// Armenia
   final Color? aM;
+
+  /// Netherlands Antilles
   final Color? aN;
+
+  /// Angola
   final Color? aO;
+
+  /// Antarctica
   final Color? aQ;
+
+  /// Argentina
   final Color? aR;
+
+  /// American Samoa
   final Color? aS;
+
+  /// Austria
   final Color? aT;
+
+  /// Australia
   final Color? aU;
+
+  /// Aruba
   final Color? aW;
+
+  /// Åland Islands
   final Color? aX;
+
+  /// Azerbaijan
   final Color? aZ;
+
+  /// Bosnia and Herzegovina
   final Color? bA;
+
+  /// Barbados
   final Color? bB;
+
+  /// Bangladesh
   final Color? bD;
+
+  /// Belgium
   final Color? bE;
+
+  /// Burkina Faso
   final Color? bF;
+
+  /// Bulgaria
   final Color? bG;
+
+  /// Bahrain
   final Color? bH;
+
+  /// Burundi
   final Color? bI;
+
+  /// Benin
   final Color? bJ;
+
+  /// Saint Barthélemy
   final Color? bL;
+
+  /// Bermuda
   final Color? bM;
+
+  /// Brunei Darussalam
   final Color? bN;
+
+  /// Bolivia
   final Color? bO;
+
+  /// Bonaire, Sint Eustatius and Saba
   final Color? bQ;
+
+  /// Brazil
   final Color? bR;
+
+  /// Bahamas
   final Color? bS;
+
+  /// Bhutan
   final Color? bT;
+
+  /// Bouvet Island
   final Color? bV;
+
+  /// Botswana
   final Color? bW;
+
+  /// Belarus
   final Color? bY;
+
+  /// Belize
   final Color? bZ;
+
+  /// Canada
   final Color? cA;
+
+  /// Cocos (Keeling) Islands
   final Color? cC;
+
+  /// Congo (the Democratic Republuc of the)
   final Color? cD;
+
+  /// Central African Republic
   final Color? cF;
+
+  /// Congo (the)
   final Color? cG;
+
+  /// Switzerland
   final Color? cH;
+
+  /// Côte d'Ivoire
   final Color? cI;
+
+  /// Cook Islands
   final Color? cK;
+
+  /// Chile
   final Color? cL;
+
+  /// Cameroon
   final Color? cM;
+
+  /// China
   final Color? cN;
+
+  /// Colombia
   final Color? cO;
+
+  /// Costa Rica
   final Color? cR;
+
+  /// Cuba
   final Color? cU;
+
+  /// Cabo Verde
   final Color? cV;
+
+  /// Curaçao
   final Color? cW;
+
+  /// Christmas Islands
   final Color? cX;
+
+  /// Cyprus
   final Color? cY;
+
+  /// Czechia
   final Color? cZ;
+
+  /// Germany
   final Color? dE;
+
+  /// Djibouti
   final Color? dJ;
+
+  /// Denmark
   final Color? dK;
+
+  /// Dominica
   final Color? dM;
+
+  /// Dominican Republic
   final Color? dO;
+
+  /// Algeria
   final Color? dZ;
+  // Ecuador
   final Color? eC;
+
+  /// Estonia
   final Color? eE;
+
+  /// Egypt
   final Color? eG;
+
+  /// Western Sahara
   final Color? eH;
+
+  /// Eritrea
   final Color? eR;
+
+  /// Spain
   final Color? eS;
+
+  /// Ethiopia
   final Color? eT;
+
+  /// Finland
   final Color? fI;
+
+  /// Fiji
   final Color? fJ;
+
+  /// Falkland Islands
   final Color? fK;
+
+  /// Micronesia
   final Color? fM;
+
+  /// Faroe Islands
   final Color? fO;
+
+  /// France
   final Color? fR;
+
+  /// Gabon
   final Color? gA;
+
+  /// United Kingdom of Great Britain and Northern Ireland
   final Color? gB;
+
+  /// Grenade
   final Color? gD;
+
+  /// Georgia
   final Color? gE;
+
+  /// French Guiana
   final Color? gF;
+
+  /// Guernsey
   final Color? gG;
+
+  /// Ghana
   final Color? gH;
+
+  /// Gibraltar
   final Color? gI;
+
+  /// Greenland
   final Color? gL;
+
+  /// Gambia
   final Color? gM;
+
+  /// Guinea
   final Color? gN;
+
+  /// Guadeloupe
   final Color? gP;
+
+  /// Equatorial Guinea
   final Color? gQ;
+
+  /// Greece
   final Color? gR;
+
+  /// South Georgia and the South Sandwich Islands
   final Color? gS;
+
+  /// Guatemala
   final Color? gT;
+
+  /// Guam
   final Color? gU;
+
+  /// Guinea-Bissau
   final Color? gW;
+
+  /// Guyana
   final Color? gY;
+
+  /// Hong Kong
   final Color? hK;
+
+  /// Heard Island and McDonald Islands
   final Color? hM;
+
+  /// Honduras
   final Color? hN;
+
+  /// Croatia
   final Color? hR;
+
+  /// Haiti
   final Color? hT;
+
+  /// Hungary
   final Color? hU;
+
+  /// Indonesia
   final Color? iD;
+
+  /// Ireland
   final Color? iE;
+
+  /// Israel
   final Color? iL;
+
+  /// Isle of Man
   final Color? iM;
+
+  /// India
   final Color? iN;
+
+  /// British Indian Ocean Territory
   final Color? iO;
+
+  /// Iraq
   final Color? iQ;
+
+  /// Iran
   final Color? iR;
+
+  /// Iceland
   final Color? iS;
+
+  /// Italy
   final Color? iT;
+
+  /// Jersey
   final Color? jE;
+
+  /// Jamaica
   final Color? jM;
+
+  /// Jordan
   final Color? jO;
+
+  /// Japan
   final Color? jP;
+
+  /// Kenya
   final Color? kE;
+
+  /// Kyrgyzstan
   final Color? kG;
+
+  /// Cambodia
   final Color? kH;
+
+  /// Kiribati
   final Color? kI;
+
+  /// Comors
   final Color? kM;
+
+  /// Saint Kitts and Nevis
   final Color? kN;
+
+  /// Korea (the Democratic People's Republic of)
   final Color? kP;
+
+  /// Korea (the Republic of)
   final Color? kR;
+
+  /// Kuwait
   final Color? kW;
+
+  /// Cayman Islands
   final Color? kY;
+
+  /// Kazakhstan
   final Color? kZ;
+
+  /// Lao People's Democratic Republic
   final Color? lA;
+
+  /// Lebanon
   final Color? lB;
+
+  /// Saint Lucia
   final Color? lC;
+
+  /// Liechenstein
   final Color? lI;
+
+  /// Sri Lanka
   final Color? lK;
+
+  /// Liberia
   final Color? lR;
+
+  /// Lesotho
   final Color? lS;
+
+  /// Lithuania
   final Color? lT;
+
+  /// Luxembourg
   final Color? lU;
+
+  /// Latvia
   final Color? lV;
+
+  /// Libya
   final Color? lY;
+
+  /// Morocco
   final Color? mA;
+
+  /// Monaco
   final Color? mC;
+
+  /// Moldova
   final Color? mD;
+
+  /// Montenegro
   final Color? mE;
+
+  ///Saint Martin (French Part)
   final Color? mF;
+
+  /// Madagascar
   final Color? mG;
+
+  /// Marshall Islands
   final Color? mH;
+
+  /// North Macedonia
   final Color? mK;
+
+  /// Mali
   final Color? mL;
+
+  /// Myanmar
   final Color? mM;
+
+  /// Mongolia
   final Color? mN;
+
+  /// Macao
   final Color? mO;
+
+  /// Northern Mariana Islands
   final Color? mP;
+
+  /// Martinique
   final Color? mQ;
+
+  /// Mauritania
   final Color? mR;
+
+  /// Montserrat
   final Color? mS;
+
+  /// Malta
   final Color? mT;
+
+  /// Mauritius
   final Color? mU;
+
+  /// Maldives
   final Color? mV;
+
+  /// Malawi
   final Color? mW;
+
+  /// Mexico
   final Color? mX;
+
+  /// Malaysia
   final Color? mY;
+
+  /// Mozambique
   final Color? mZ;
+
+  /// Namibia
   final Color? nA;
+
+  /// New Caledonia
   final Color? nC;
+
+  /// Niger
   final Color? nE;
+
+  /// Norfolk Island
   final Color? nF;
+
+  /// Nigeria
   final Color? nG;
+
+  /// Nicaraqua
   final Color? nI;
+
+  /// Netherlands
   final Color? nL;
+
+  /// Norway
   final Color? nO;
+
+  /// Nepal
   final Color? nP;
+
+  /// Nauru
   final Color? nR;
+
+  /// Niue
   final Color? nU;
+
+  /// New Zealand
   final Color? nZ;
+
+  /// Oman
   final Color? oM;
+
+  /// Panama
   final Color? pA;
+
+  /// Peru
   final Color? pE;
+
+  /// French Polynesia
   final Color? pF;
+
+  /// Papa New Guinea
   final Color? pG;
+
+  /// Philippines
   final Color? pH;
+
+  /// Pakistan
   final Color? pK;
+
+  /// Poland
   final Color? pL;
+
+  /// Saint Pierre and Miquelon
   final Color? pM;
+
+  /// Pitcairn
   final Color? pN;
+
+  /// Puerto Rico
   final Color? pR;
+
+  /// Palestine
   final Color? pS;
+
+  /// Portugal
   final Color? pT;
+
+  /// Palau
   final Color? pW;
+
+  /// Paraguay
   final Color? pY;
+
+  /// Qatar
   final Color? qA;
+
+  /// Réunion
   final Color? rE;
+
+  /// Romania
   final Color? rO;
+
+  /// Serbia
   final Color? rS;
+
+  /// Russian Federation
   final Color? rU;
+
+  /// Rwanda
   final Color? rW;
+
+  /// Saudi Arabia
   final Color? sA;
+
+  /// Solomon Islands
   final Color? sB;
+
+  /// Seychelles
   final Color? sC;
+
+  /// Sudan
   final Color? sD;
+
+  /// Sweden
   final Color? sE;
+
+  /// Singapore
   final Color? sG;
+
+  /// Saint Helena, Ascension and Tristan de Cunha
   final Color? sH;
+
+  /// Slovenia
   final Color? sI;
+
+  /// Svalbard and Jan Mayen
   final Color? sJ;
+
+  /// Slovakia
   final Color? sK;
+
+  /// Sierra Leone
   final Color? sL;
+
+  /// San Marino
   final Color? sM;
+
+  /// Senegal
   final Color? sN;
+
+  /// Somalia
   final Color? sO;
+
+  /// Suriname
   final Color? sR;
+
+  /// South Sudan
   final Color? sS;
+
+  /// Sao Tome
   final Color? sT;
+
+  ///El Salvador
   final Color? sV;
+
+  /// Sint Maarten (Dutch part)
   final Color? sX;
+
+  /// Syrian Arab Republic
   final Color? sY;
+
+  /// Eswatini
   final Color? sZ;
+
+  ///Turks and Caicos Islands
   final Color? tC;
+
+  /// Chad
   final Color? tD;
+
+  /// French Southern Territories
   final Color? tF;
+
+  /// Togo
   final Color? tG;
+
+  /// Thailand
   final Color? tH;
+
+  /// Tajikistan
   final Color? tJ;
+
+  /// Tokelau
   final Color? tK;
+
+  /// Timor-Leste
   final Color? tL;
+
+  /// Turkmenistan
   final Color? tM;
+
+  /// Tunisia
   final Color? tN;
+
+  /// Tonga
   final Color? tO;
+
+  /// Turkey
   final Color? tR;
+
+  /// Trinidad and Tobago
   final Color? tT;
+
+  /// Tuvalu
   final Color? tV;
+
+  /// Taiwan
   final Color? tW;
+
+  /// Tanzania, the United REpublic of
   final Color? tZ;
+
+  /// Ukraine
   final Color? uA;
+
+  /// Uganda
   final Color? uG;
+
+  /// United States Minor Outlying Islands
   final Color? uM;
+
+  /// United States of America
   final Color? uS;
+
+  /// Uruguay
   final Color? uY;
+
+  /// Uzbekistan
   final Color? uZ;
+
+  /// Holy See
   final Color? vA;
+
+  /// Saint Vincent and the Grenadines
   final Color? vC;
+
+  /// Venezuela
   final Color? vE;
+
+  /// Virgin Islands (British)
   final Color? vG;
+
+  /// Virgin Islands (U.S.)
   final Color? vI;
+
+  /// Viet Nam
   final Color? vN;
+
+  /// Vanuatu
   final Color? vU;
+
+  /// Wallis and Futuna
   final Color? wF;
+
+  /// Samoa
   final Color? wS;
+
+  /// Kosovo
   final Color? xK;
+
+  /// Yemen
   final Color? yE;
+
+  /// Mayotte
   final Color? yT;
+
+  /// South Africa
   final Color? zA;
+
+  /// Zambia
   final Color? zM;
+
+  /// Zimbabwe
   final Color? zW;
 
   const CountryColors({
